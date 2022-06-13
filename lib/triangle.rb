f1699184a05b24994c99492dd2fd834b601ca5d4
Emma Perlman <emmajperlman@gmail.com>
2022-06-12 19:10:56 -0700

    commit tree b5fd4d5b141cfab3005e77c8be082ce97f8b4416

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :l1,:l2, :l3
+  def initialize(l1,l2,l3)
+    @l1=l1
+    @l2=l2
+    @l3=l3
+  end
+  def kind
+    if l1>0 && l2>0 && l3>0 && l1+l2>l3 && l2+l3>l1 && l3+l1>l2
+      if l1==l2 && l2==l3
+        @kind= :equilateral
+      elsif l1!=l2 && l2!=l3 && l3!=l1
+        @kind=:scalene
+      else @kind=:isosceles
+      end
+    else 
+      raise TriangleError
+    end
+  end
+
+  class TriangleError<StandardError
+  end
+
 end
+
+tri=Triangle.new(1,2,3)
+binding.pry
